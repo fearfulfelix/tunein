18ed45f0cca000e0196dd908bd542b4636b512b2
AlexEOHare <alexeohare@gmail.com>
2021-02-04 12:34:07 -0500

    Updating Top Bar

--- TuneIn Website/tunein/user_profile/views.py
+++ TuneIn Website/tunein/user_profile/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from django.template import loader
 from .models import *
@@ -19,3 +19,5 @@ def index(request):
 
     template = loader.get_template('user_profile/index.html')
     return HttpResponse(template.render(con))
+
+
