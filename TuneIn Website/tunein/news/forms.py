37b7cc06845b233e4db2e50e8687a8368ee7a9ad
Félix Turgeon <42982058+fearfulfelix@users.noreply.github.com>
2021-04-06 22:35:35 -0400

    Added Shaynes work

--- TuneIn Website/tunein/news/forms.py
+++ TuneIn Website/tunein/news/forms.py
@@ -5,7 +5,7 @@ from .models import Comment, Post
 class NewPostForm(forms.ModelForm):
     class Meta:
         model = Post
-        fields = ['description', 'pic', 'tags']
+        fields = ['description', 'pic', 'choose_file' ,'tags']
         widgets={'description':forms.Textarea(attrs={'cols':50})}
 
 
